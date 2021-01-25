99d6c67e138cbd79d67b0903dea6cf9929f52f40
Anantha Raju C <arcswdev@gmail.com>
2021-01-25 15:52:59 +0530

    Initial commit

---
+++ src/main/java/io/github/anantharajuc/bookmarc/BookmarcApplication.java
@@ -0,0 +1,13 @@
+package io.github.anantharajuc.bookmarc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookmarcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookmarcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/anantharajuc/bookmarc/BookmarcApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.anantharajuc.bookmarc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
